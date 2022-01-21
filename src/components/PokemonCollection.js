a567ef2852107157642fd118c06563a3cb4a680e
Ashley Mattam <amattam427@gmail.com>
2022-01-20 23:07:58 -0500

    commit tree 02d196a35dfc378df418056f74c7073115f00036

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,12 +2,24 @@ import React from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection() {
+function PokemonCollection({pokemon}) {
   return (
     <Card.Group itemsPerRow={6}>
-      <h1>Hello From Pokemon Collection</h1>
+      <h1>Pokemon Collection</h1>
+      {pokemon.map((p)=>
+        <PokemonCard
+          key={p.id}
+          id={p.id}
+          name={p.name}
+          hp={p.hp}
+          sprites={p.sprites}
+        />
+      )}
     </Card.Group>
   );
 }
 
 export default PokemonCollection;
+
+
+
--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -1,29 +1,68 @@
-import React from "react";
+import React, {useState} from "react";
 import { Form } from "semantic-ui-react";
 
-function PokemonForm() {
+function PokemonForm({addPokemon}) {
+  const [formData, setFormData] = useState({
+    //name, hp, front/backUrl are all part of the attributes inside the inputs
+    name:"",
+    hp:"",
+    frontUrl: "",
+    backUrl: "",
+  })
+
+  function handleSubmit(e){
+    e.preventDefault();
+    const newPokemon={
+      name: formData.name,
+      hp: formData.hp,
+      sprites:{
+        front: formData.frontUrl,
+        back: formData.backUrl,
+      },
+    };
+
+    fetch('http://localhost:3001/pokemon',{
+      method: 'POST',
+      headers: {
+        'Content-Type' : 'application/json',
+      },
+      body: JSON.stringify(newPokemon)
+    })
+    .then((r)=>r.json())
+    .then(addPokemon);
+  }
+
+  function handleChange(e){
+    setFormData({
+      ...formData,
+      [e.target.name] : e.target.value
+    })
+  }
+
   return (
     <div>
       <h3>Add a Pokemon!</h3>
       <Form
-        onSubmit={() => {
-          console.log("submitting form...");
-        }}
+        onSubmit={handleSubmit}
       >
         <Form.Group widths="equal">
-          <Form.Input fluid label="Name" placeholder="Name" name="name" />
-          <Form.Input fluid label="hp" placeholder="hp" name="hp" />
+          <Form.Input fluid label="Name" placeholder="Name" name="name" value={formData.name} onChange={handleChange}/>
+          <Form.Input fluid label="hp" placeholder="hp" name="hp" value={formData.hp} onChange={handleChange}/>
           <Form.Input
             fluid
             label="Front Image URL"
             placeholder="url"
             name="frontUrl"
+            value={formData.frontUrl}
+            onChange={handleChange}
           />
           <Form.Input
             fluid
             label="Back Image URL"
             placeholder="url"
             name="backUrl"
+            value={formData.backUrl}
+            onChange={handleChange}
           />
         </Form.Group>
         <Form.Button>Submit</Form.Button>
@@ -33,3 +72,4 @@ function PokemonForm() {
 }
 
 export default PokemonForm;
+
